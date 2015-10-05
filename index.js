4ddd31d2fe21bccf16047af89bb3421ff5ebb1ed
redjellyfish.co.uk <dev@redjellyfish.co.uk>
2015-10-05 07:24:33 +0100

    Methods for calling engine.process

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-module.exports = function (width, height) {
-    return width * height;
+var engine = require('./lib/engine.js');
+var fs = require('fs');
+var htmlparser = require("htmlparser2");
+var domutils = require('domutils');
+
+function renderDOM(dom, model) {
+    return domutils.getOuterHTML(engine.process(dom, model));
+}
+
+function renderHTML(html, model) {
+    return renderDOM(htmlparser.parseDOM(html), model);
+}
+
+function renderFile(path, model) {
+    return this.renderHTML(fs.readFileSync(file, "utf8"), model);
+}
+
+module.exports = {
+    renderDOM: renderDOM,
+    renderHTML: renderHTML,
+    renderFile: renderFile
 };
